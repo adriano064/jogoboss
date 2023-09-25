10d8b027c092156238686682aad39f430ff4567b
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-09-25 11:34:08 -0300

    Movimentação do Boss - Golem (Em andamento...)

--- jogoboss/Assets/Golem/Script/Golem.cs
+++ jogoboss/Assets/Golem/Script/Golem.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class Golem : MonoBehaviour
 {
+    public float speed;
+    public float walkTime;
+    public bool walkRight = true;
+
+    private float timer;
+
+    private Rigidbody2D rig;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.left * speed;
+        }
     }
 }
