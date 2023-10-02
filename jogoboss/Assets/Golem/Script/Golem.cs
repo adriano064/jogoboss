2c3ee25f2ec9a1df4a060c6ea52accb20a7066ad
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-10-02 11:41:39 -0300

    Movimentação do Boss - Golem (Em andamento...) - [parte 2]

--- jogoboss/Assets/Golem/Script/Golem.cs
+++ jogoboss/Assets/Golem/Script/Golem.cs
@@ -6,40 +6,28 @@ using Vector2 = UnityEngine.Vector2;
 
 public class Golem : MonoBehaviour
 {
-    public float speed;
-    public float walkTime;
-    public bool walkRight = true;
+    public Transform player;
 
-    private float timer;
+    public bool isFlipped = false;
 
-    private Rigidbody2D rig;
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f,180f,0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f,180f,0f);
+            isFlipped = true;
+        }
+    }
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        rig = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        timer += Time.deltaTime;
-
-        if (timer >= walkTime)
-        {
-            walkRight = !walkRight;
-            timer = 0f;
-        }
-
-        if (walkRight)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-            rig.velocity = Vector2.right * speed;
-        }
-        else
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-            rig.velocity = Vector2.left * speed;
-        }
-    }
+    
 }
