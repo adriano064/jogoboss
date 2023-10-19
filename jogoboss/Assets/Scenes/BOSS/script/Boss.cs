6c93bc044e4d38049fc76ecccbbf49de581934cc
Georgia Paulina Pereira Bezerra <20211170150026@ifrn.local>
2023-10-19 09:56:04 -0300

    criando inimigo part1

--- jogoboss/Assets/Scenes/BOSS/script/Boss.cs
+++ jogoboss/Assets/Scenes/BOSS/script/Boss.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rig;
+    public float walkTime;
+    private float timer;
+    public bool walkRight = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        timer += Time.deltaTime;
+
+        if(timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
         
     }
 }
